8bc1c833073f2fc8e00435d863094bd642d37bff
Sahil Mor <43886039+sahil-mor@users.noreply.github.com>
2020-02-22 02:53:41 +0530

    removing api key

--- models/ScheduleOf.js
+++ models/ScheduleOf.js
@@ -1,5 +1,5 @@
 var request = require('request')
-var apiKey = "xbdeqe5aq3zb6t8rfezgw65f"
+var apiKey = ""
 schedule = (req,res)=>{
     schedule = "http://api.sportradar.us/cricket-t2/en/teams/" + req.params.id + "/schedule.json?api_key=" + apiKey
     request(schedule,function(err,response,body){
